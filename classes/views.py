2efbe447bbca93bca90b6bc61096d252a44ff571
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2025-05-27 08:21:24 +0500

    Added the CRUD views for the class Model

--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import StudentClass
+from .forms import StudentClassForm
 
-# Create your views here.
+
+class StudentClassListView(ListView):
+    """Displays all class objects."""
+    model = StudentClass
+    context_object_name = 'classes'
+    # template_name = 'classes/class_list.html'
+
+
+class StudentClassDetailView(DetailView):
+    """Displays details of a single class."""
+    model = StudentClass
+    context_object_name = 'class_obj'
+    # template_name = 'classes/class_detail.html'
+
+
+class StudentClassCreateView(CreateView):
+    """Handles creating a new class using a form."""
+    model = StudentClass
+    form_class = StudentClassForm
+    success_url = reverse_lazy('class-list')
+    # template_name = 'classes/class_form.html'
+
+
+# Uncomment this when implement it
+# class StudentClassUpdateView(UpdateView):
+#     """Handles updating an existing class."""
+#     model = StudentClass
+#     form_class = StudentClassForm
+#     success_url = reverse_lazy('class-list')
+#     template_name = 'classes/class_form.html'
+
+
+class StudentClassDeleteView(DeleteView):
+    """Handles deleting a class."""
+    model = StudentClass
+    success_url = reverse_lazy('class-list')
+    # template_name = 'classes/class_confirm_delete.html'
