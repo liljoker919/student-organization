5f266cea64110c9dd07ef7ee1701bd711d213ae4
liljoker919 <chrs.nickerson@gmail.com>
2025-05-29 21:54:21 -0400

    Enhance home page layout and add demo view with styling updates

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
+from django.urls import path
 from django.shortcuts import render
 
 
+# Views
 def home_page(request):
-    return render(request,'core/home.html')
+    return render(request, "core/home.html")
+
+
+def demo_view(request):
+    return render(request, "core/demo.html")
